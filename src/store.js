d046476bf66da37e7d786dd1d0189ec296eebc2b
Jef Fisher <jfisher@tevora.com>
2018-09-10 13:14:24 -0700

    mutations

--- src/store.js
+++ src/store.js
@@ -7,10 +7,10 @@ export const store = new Vuex.Store({
     state: {
         registrations: [],
         users: [
-            {id: 1, name: 'Max', registered: false},
-            {id: 2, name: 'Anna', registered: false},
-            {id: 3, name: 'Chris', registered: false},
-            {id: 4, name: 'Sven', registered: false}
+            {id: 1, name: 'Grizzly', registered: false},
+            {id: 2, name: 'Ronen', registered: false},
+            {id: 3, name: 'Lainie', registered: false},
+            {id: 4, name: 'Remy', registered: false}
         ]
     },
     getters: {
@@ -24,6 +24,31 @@ export const store = new Vuex.Store({
         },
         totalRegistrations(state) {
             return state.registrations.length;
+        }
+    },
+    mutations: {
+        register(state, userId) {
+            const date = new Date;
+            const user = state.users.find(user => {
+                return user.id === userId
+            });
+            user.registered = true;
+            const registration = {
+                userId: userId,
+                name: user.name,
+                date: date.getMonth() + '/' + date.getDay()
+            };
+            state.registrations.push(registration);
+        },
+        unregister(state, payload) {
+            const user = state.users.find(user => {
+                return user.id === payload.userId
+            });
+            user.registered = false;
+            const registration = state.registrations.find(registration => {
+                return registration.userId === payload.userId;
+            });
+            state.registrations.splice(state.registrations.indexOf(registration), 1);
         }
     }
 });
